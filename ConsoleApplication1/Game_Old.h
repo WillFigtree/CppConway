2ee9e0a721fe42c654f3365d46c265f3579f9472
WillFigtree <w.figtree@gmail.com>
2020-01-30 16:04:12 +1100

    Removed old semi-C style game logic.

--- ConsoleApplication1/Game_Old.h
+++
@@ -1,9 +0,0 @@
-//#pragma once
-//#include <vector>
-//
-//void InitialiseGame(uint8_t gameSize, std::vector<std::vector<bool>> &gameState);
-//void ReleaseGame(std::vector<std::vector<bool>>& gameState);
-//void UpdateGame(std::vector<std::vector<bool>> &gameState);
-//void SeedBlinker(uint8_t x, uint8_t y, std::vector<std::vector<bool>> &gameState);
-//void SeedBeacon(uint8_t x, uint8_t y, std::vector<std::vector<bool>>& gameState);
-//void SeedGlider(uint8_t x, uint8_t y, std::vector<std::vector<bool>>& gameState);
